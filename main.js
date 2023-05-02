df88e44ad50d87e6f7c861ea80a6959f7b5a710f
Yutsuki Zaitsu <89077321+YuiZaitsu@users.noreply.github.com>
2023-05-02 14:41:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 Status = "";
 objects = [];
+song="";
+
+function preload(){
+    song=loadSound("ram_siya_ram.mp3");
+
+}
 
 function setup(){
     canvas = createCanvas(480,380);
@@ -7,6 +13,8 @@ function setup(){
     video = createCapture(VIDEO);
     video.size(480,380);
     video.hide();
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Object";
 }
 
 function modelLoaded(){
@@ -14,7 +22,7 @@ function modelLoaded(){
     Status = true;
 }
 function draw(){
-    image(video,0,0,300,290);
+    image(video,0,0,480,380);
     if(Status != ""){
 
         object_Detector.detect(video, gotResults);
@@ -33,15 +41,22 @@ function draw(){
             stroke("Black");
             rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
 
-            if(objects[i].label == input_text){
-                video.stop();
-                object_Detector.detect(gotResults);
-                document.getElementById("object_found").innerHTML = input_text+" Found";
+            if(objects[i].label == "person"){
+                song.stop();
+                
+                document.getElementById("object_found").innerHTML = " Baby found";
                 
             }
             else{
-                document.getElementById("object_found").innerHTML = input_text + " Not Found";
+                song.play();
+
+                document.getElementById("object_found").innerHTML =  "  Baby Not Found";
             }
+        }
+        if(objects.length == 0){
+            song.play();
+
+                document.getElementById("object_found").innerHTML =  "  Baby Not Found";
         }
     }
 }
@@ -57,8 +72,3 @@ function gotResults(error,results){
 }
 
 
-function start(){
-    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
-    document.getElementById("status").innerHTML = "Status: Detecting Object";
-   
-}
